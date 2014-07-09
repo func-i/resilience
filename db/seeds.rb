bbe687a4a1e32f95636c0c6dc2a9b0d04d284a22
Anton Yordanov <anton.yordanov@droxic.com>
2014-07-09 09:22:15 +0300

    seed some users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'factory_girl'
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+FactoryGirl.create :user,
+  email: 'anton.yordanov@droxic.com',
+  password: 'password'
+
+FactoryGirl.create :user,
+  email: 'anton.yordanov@gmail.com',
+  password: 'password'
